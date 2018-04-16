546166ba2412878cf3442274706c676ecb93e57b
Bella Vanillo and semhar kidane <semhar@>
2018-04-16 16:45:18 -0700

    extra

---
+++ js/scripts.js
@@ -0,0 +1,173 @@
+$(document).ready(function(){
+  $(".card1").click(function(){
+    //
+    $("#pic2").hide();
+    $("#pic1").show();
+
+
+    $("#pic3").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic4").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card2").click(function(){
+    $("#pic4").hide();
+    $("#pic3").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card3").click(function(){
+    $("#pic6").hide();
+    $("#pic5").show();
+
+    $("#pic1").hide();
+    $("#pic3").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic4").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card4").click(function(){
+    $("#pic8").hide();
+    $("#pic7").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic3").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic4").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card5").click(function(){
+    $("#pic10").hide();
+    $("#pic9").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic3").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic4").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card6").click(function(){
+    $("#pic12").hide();
+    $("#pic11").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic3").hide();
+    $("#pic13").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic4").show();
+    $("#pic14").show();
+    $("#pic16").show();
+
+  });
+  $(".card7").click(function(){
+    debugger;
+    $("#pic14").hide();
+    $("#pic13").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic3").hide();
+    $("#pic15").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic4").show();
+    $("#pic16").show();
+
+  });
+  $(".card8").click(function(){
+    $("#pic16").hide();
+    $("#pic15").show();
+
+    $("#pic1").hide();
+    $("#pic5").hide();
+    $("#pic7").hide();
+    $("#pic9").hide();
+    $("#pic11").hide();
+    $("#pic13").hide();
+    $("#pic3").hide();
+
+    $("#pic2").show();
+    $("#pic6").show();
+    $("#pic8").show();
+    $("#pic10").show();
+    $("#pic12").show();
+    $("#pic14").show();
+    $("#pic4").show();
+
+  });
+});
